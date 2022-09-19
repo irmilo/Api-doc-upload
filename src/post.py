9d4562ef86be2ab738f9109b55aaa741ee1fce02
irmilo <maxim.olegovich.p@gmail.com>
2022-09-20 02:50:53 +0300

    parser-upload

--- src/post.py
+++ src/post.py
@@ -1,7 +1,8 @@
 import requests
 import hashlib
 import os
-
+import csv
+import xml.etree.ElementTree as ET
 
 success = '<Response [200]>'
 fail = '<Response [400]>'
@@ -20,6 +21,53 @@ def check_file(filename):
         #rest api
 
 
+def pars(filename):
+    f = open(filename, encoding='utf-8')
+    csv_f = csv.reader(f, delimiter=";")
+    data = ""
+    count = 0
+    for row in csv_f:
+        if count == 0:
+            count += 1
+            continue
+        id = get_product(row[1])
+        if id is None:
+            continue
+        data += convert_row([id, row[3], row[6]])
+    f.close()
+    return data
+
+
+def convert_row(row):
+    return """\t  <item>
+                  <discountSum>0</discountSum>
+                  <sum>%s</sum>
+                  <amount>%s</amount>
+                  <productId>%s</productId>
+                  <productArticle>0</productArticle>
+                  <storeId>%s</storeId> 
+          </item>\n""" % (row[2], row[1], row[0], store_id)
+
+
+def get_store():
+    gets_url = (protocol + '://' + server + ':' + port + bd + '/api/corporation/stores?key=' + token.text)
+    store = requests.get(gets_url)  # спарсить store, чтобы получить guid
+    root = ET.fromstring(store.content)
+    return root[0][0].text
+
+
+def get_product(name):
+    getp_url = (protocol + '://' + server + ':' + port + bd + '/api/products/search?key=' + token.text + '&name='
+                + name)
+    product = requests.get(getp_url)  # спарсить product, чтобы получить guid
+    root = ET.fromstring(product.content)
+    ty = root.find('productDto')
+    if ty is None:
+        return None
+    else:
+        return root[0][0].text
+
+
 # Хеширование пароля в sha1/отправка запроса на получение токена
 def auth():
     sha1pass = hashlib.sha1(password.encode('utf-8')).hexdigest()
@@ -29,9 +77,8 @@ def auth():
 
 
 # Формирование и отправка запроса на выгрузку xml
-def upload(token):
+def upload(token, xml):
     post_url = (protocol + '://' + server + ':' + port + bd + '/api/documents/import/salesDocument?key=' + token.text)
-    xml = open(filename, 'rb').read().decode('utf-8')
     headers = {'Content-Type': 'application/xml'}  # set what your server accepts
     answer = str(requests.post(post_url, data=xml, headers=headers))
     result(answer)
@@ -64,5 +111,17 @@ while not file_flag:
     filename = input("Введите название загружаемого файла: ")
     file_flag = check_file(filename)
 token = auth()
-upload(token)
+store_id = get_store()
+data = """<document>
+  <items>\n"""
+data += pars(filename)
+data += """  </items>
+  <status>NEW</status>
+  <accountToCode>5.01</accountToCode>
+  <revenueAccountCode>4.01</revenueAccountCode>
+  <documentNumber>%s</documentNumber>
+  <dateIncoming>19.09.2022</dateIncoming>
+</document>""" % (filename)
+print(data)
+upload(token, data)
 logout(token)
